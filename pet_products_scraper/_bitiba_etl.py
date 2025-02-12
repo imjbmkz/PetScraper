4e421e6acdbd73b66244e0873a6bbc7d21d035a0
Josh Dev <imjbmkz@gmail.com>
2025-02-12 23:46:52 +0800

    Modified Bitiba transform

--- pet_products_scraper/_bitiba_etl.py
+++ pet_products_scraper/_bitiba_etl.py
@@ -20,82 +20,49 @@ class BitibaETL(PetProductsETL):
     
     def transform(self, soup: BeautifulSoup, url: str):
 
-        if soup:
-        
-            # Get product wrappers. Each wrapper may have varying content.
-            product_wrappers = soup.select('div[class*="ProductCard_productCard"]')
+        try:
 
-            # Placeholder for consolidated data frames
-            consolidated_data = []
+            if soup:
+            
+                product_data_list = soup.select("script[type*='application/ld+json']")
+                if product_data_list:
+                    product_data = json.loads(product_data_list[0].text)
 
-            # Iterate through the wrappers
-            for wrapper in product_wrappers:
+                    product_title = product_data["name"]
+                    rating = None
+                    if "aggregateRating" in product_data.keys():
+                        rating = product_data["aggregateRating"]["ratingValue"]
+                        rating = f"{rating}/5"
+                    description = product_data["description"]
+                    product_url = url.replace(self.BASE_URL, "")
 
-                # Get the product title, rating, and description
-                product_title = wrapper.select_one('a[class*="ProductListItem_productInfoTitleLink"]').text
-                rating = wrapper.select_one('span[class*="pp-visually-hidden"]').text
-                description = wrapper.select_one('p[class*="ProductListItem_productInfoDescription"]').text
-                product_url = wrapper.select_one('a[class*="ProductListItem_productInfoTitleLink"]')["href"]
+                    # Placeholder for variant details
+                    variants = []
+                    prices = []
+                    discounted_prices = []
+                    discount_percentages = []
 
-                # Get product variants. Each variant has their own price.
-                product_variants = wrapper.select('div[class*="ProductListItemVariant_variantWrapper"]')
+                    for offer in product_data["offers"]:
 
-                # Placeholder for variant details
-                variants = []
-                prices = []
-                discounted_prices = []
-                discount_percentages = []
+                        reference_value = offer["priceSpecification"]["referenceQuantity"]["value"]
+                        reference_unit = offer["priceSpecification"]["referenceQuantity"]["unitCode"]
+                        variant = f"{reference_value} {reference_unit}"
+                        price = offer["priceSpecification"]["price"]
 
-                # Get the variant name, price, and reference price
-                for variant in product_variants:
-                    variants.append(variant.select_one('span[class*="ProductListItemVariant_variantDescription"]').text)
-
-                    price = float(variant.select_one('span[class*="z-price__amount"]').text.replace("£", ""))
-
-                    # Not all products are discounted. Sometimes, there are no reference prices
-                    reference_price_span = variant.select_one('span[data-zta*="productReducedPriceRefPriceAmount"]')
-                    if reference_price_span:
-
-                        # Reference price is the original price before discount
-                        reference_price = float(reference_price_span.text.replace("£", ""))
-                        prices.append(reference_price)
-                        discounted_prices.append(price)
-
-                        # Calculate the discount percentage
-                        discount_percentage = ((reference_price - price) / reference_price) 
-                        discount_percentages.append(discount_percentage)
-
-                    else:
-                        # If there is no reference price, then the product is not discounted
+                        variants.append(variant)
                         prices.append(price)
-                        discounted_prices.append(None)
-                        discount_percentages.append(None)
 
-                
-                # Compile the data acquired into dataframe
-                df = pd.DataFrame(
-                    {
-                        "variant": variants, 
-                        "price": prices,
-                        "discounted_price": discounted_prices,
-                        "discount_percentage": discount_percentages
-                    }
-                )
-                df.insert(0, "url", product_url)
-                df.insert(0, "description", description)
-                df.insert(0, "rating", rating)
-                df.insert(0, "name", product_title)
+                    df = pd.DataFrame({"variant": variants, "price": prices})
+                    df.insert(0, "url", product_url)
+                    df.insert(0, "description", description)
+                    df.insert(0, "rating", rating)
+                    df.insert(0, "name", product_title)
+                    df.insert(0, "shop", self.SHOP)
 
-                consolidated_data.append(df)
-            
-            try:
-                df_consolidated = pd.concat(consolidated_data, ignore_index=True)
-                df_consolidated.insert(0, "shop", self.SHOP)
-
-                return df_consolidated
-            
-            except Exception as e:
-                logger.error(f"Error scraping {url}: {e}")
+                    return df
+        
+        except Exception as e:
+            logger.error(f"Error scraping {url}: {e}")
 
     def get_links(self, category: str) -> pd.DataFrame:
         # Data validation on category
@@ -147,41 +114,3 @@ class BitibaETL(PetProductsETL):
             df.insert(0, "shop", self.SHOP)
 
             return df
-
-    def run(self, db_conn: Engine, table_name: str):
-
-        sql = get_sql_from_file("select_unscraped_urls.sql")
-        sql = sql.format(shop=self.SHOP)
-        df_urls = self.extract_from_sql(db_conn, sql)
-
-        for i, row in df_urls.iterrows():
-
-            pkey = row["id"]
-            url = row["url"]
-
-            while True:
-
-                now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-                soup = self.extract_from_url("GET", url)
-                df = self.transform(soup, url)
-
-                if df is not None:
-                    self.load(df, db_conn, table_name)
-                    
-                else:
-                    update_url_scrape_status(db_conn, pkey, "FAILED", now)
-
-                if soup:
-                    # Repeat the process if there are new pages
-                    next_page_a = soup.find("a", attrs={"data-zta": "paginationNext"})
-                    if next_page_a:
-                        url = next_page_a["href"]
-                    else:
-                        break
-                
-            if df is not None:
-                update_url_scrape_status(db_conn, pkey, "DONE", now)
-            
-            else:
-                update_url_scrape_status(db_conn, pkey, "FAILED", now)
